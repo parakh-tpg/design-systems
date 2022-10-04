dfdd0afb5d4c7eede2d203f070cb9db68fd2846f
Parakh Maheshwari <parakh.maheshwari@3pillarglobal.com>
2022-10-04 16:03:57 +0530

    Added support to render empty label to select component

--- src/components/input/select/BasicSelect.js
+++ src/components/input/select/BasicSelect.js
@@ -1,12 +1,12 @@
-import * as React from 'react';
-import Box from '@mui/material/Box';
-import InputLabel from '@mui/material/InputLabel';
-import MenuItem from '@mui/material/MenuItem';
-import FormControl from '@mui/material/FormControl';
-import Select from '@mui/material/Select';
+import * as React from "react";
+import Box from "@mui/material/Box";
+import InputLabel from "@mui/material/InputLabel";
+import MenuItem from "@mui/material/MenuItem";
+import FormControl from "@mui/material/FormControl";
+import Select from "@mui/material/Select";
 
 export default function BasicSelect() {
-  const [age, setAge] = React.useState('');
+  const [age, setAge] = React.useState("");
 
   const handleChange = (event) => {
     setAge(event.target.value);
@@ -14,15 +14,17 @@ export default function BasicSelect() {
 
   return (
     <Box sx={{ minWidth: 120 }}>
-      <FormControl style={{width:'120px'}}>
-        <InputLabel id="demo-simple-select-label">Age</InputLabel>
+      <FormControl style={{ width: "120px" }}>
         <Select
           labelId="demo-simple-select-label"
           id="demo-simple-select"
           value={age}
-          label="Age"
           onChange={handleChange}
+          displayEmpty
         >
+          <MenuItem disabled value="">
+            <em>Select</em>
+          </MenuItem>
           <MenuItem value={10}>Ten</MenuItem>
           <MenuItem value={20}>Twenty</MenuItem>
           <MenuItem value={30}>Thirty</MenuItem>
